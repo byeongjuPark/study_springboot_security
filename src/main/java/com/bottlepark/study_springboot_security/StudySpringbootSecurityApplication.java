9c5af0b35960dca19292abe7eb5b6316296837ed
byeongjuPark <bottlepark28@gmail.com>
2023-02-08 12:42:30 +0900

    first commit

---
+++ src/main/java/com/bottlepark/study_springboot_security/StudySpringbootSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.bottlepark.study_springboot_security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudySpringbootSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudySpringbootSecurityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bottlepark/study_springboot_security/StudySpringbootSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bottlepark.study_springboot_security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudySpringbootSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
